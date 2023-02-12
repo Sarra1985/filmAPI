6a2b99c3374aaae258555bee5d2d129026f47e1e
Sarra1985 <110898069+Sarra1985@users.noreply.github.com>
2023-02-12 22:46:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const form = document.getElementById("searchForm");
+const searchInput = document.getElementById("searchInput");
+const result = document.getElementById("result");
+
+let search = "";
+let movies = [];
+
+const fetchMovies = async () => {
+    movies = await fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=fc35fa2ae200a1b66133d6fbd9bac889&query=${search}`
+    ).then((res) => res.json());
+    console.log(movies);
+};
+
+const moviesDisplay = async () => {
+    await fetchMovies();
+
+    movies.results.length = 12;
+
+    result.innerHTML = movies.results
+        .map(
+            (movie) =>
+                `
+            <li>
+                <h2>${movie.original_title}</h2>
+                <div class="card-content">
+                    <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}"></img>
+                    <div class="infos">
+                        <p>${movie.overview}</p>
+                        <p>Popularité : ${movie.popularity} ⭐️</p>
+                    </div>
+                </div>
+            </li>
+        `
+        )
+        .join("");
+};
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    search = searchInput.value;
+    moviesDisplay();
+});
